a58db7576fdec48201b95d1d9dd939f27f9ba43c
Richard Honiker <richard.honiker@mymail.champlain.edu>
2020-02-08 18:54:35 -0500

    Couldn't figure out what's wrong with the slideshow,  but it's definitely in the JS's style, line 91.

--- locate.js
+++ locate.js
@@ -3,11 +3,11 @@
 var key = "HB4E0LPBodtgXJlBVOYvZSDaxgGSCA7Li7Eq6tqb6uVDRfzAp4";
 var secret = "ACCq1YmJ2XzZxT6WrvbSU9voepnbCllw0NmSF363";
 
-var pf = new petfinder.Client({apiKey: key, secret: secret});
+/*var pf = new petfinder.Client({apiKey: key, secret: secret});
 
 pf.authenticate()
 {
-  /* MAY NEED TO IMPLEMENT LATER */
+  // MAY NEED TO IMPLEMENT LATER 
 }
 
 pf.animal.search()
@@ -20,7 +20,7 @@ pf.animal.search()
 		document.body.style.backgroundColor = "red";
 
 
-	});
+	});*/
 
 display = document.getElementById("display");
 
@@ -85,23 +85,22 @@ function currentSlide(n)
 function showSlides(n)
 {
 	var i;
-	var slides = document.getElementsByClassName("slideshow");
+	var slides = document.getElementsByClassName("petSlideshow");
 	if (n > slides.length) {slideIndex = 1}
 	if (n < 1) {slideIndex = slides.length}
-	for (i = 0; i < slides.length; i++)
-	{
-		slides[i].style.display = "none";
+	for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
 	}
 	slides[slideIndex-1].style.display = "block";
 }
 
 /* Un-comment this to make the slides automatically scroll */
-/* 
+/*
 var slideIndex = 0;
 showSlides();
 function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("petSlideshow");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -109,5 +108,4 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-*/
+}*/
